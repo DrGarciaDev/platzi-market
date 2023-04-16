a64a3d764c0ca2cd1eae10b0c8fbcb69220983d9
DrGarciaDev <luis_grdz@hotmail.com>
2023-04-16 14:43:33 -0600

    Orientando nuestra API a dominio video 21: https://platzi.com/clases/1996-java-spring/31510-que-es-el-patron-data-mapper-y-que-resuelve/

---
+++ src/main/java/com/platzi/market/domain/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.platzi.market.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.platzi.market.domain.Product;
+
+public interface ProductRepository {
+//	En este repositorio solo se ponen los nombres de los métodos
+//	que queremos que cualquier repositorio que trabaje con productos
+//	tenga que implementar 
+	
+	List<Product> getAll();
+	Optional<List<Product>> getByCategory(int categoryId);
+	Optional<List<Product>> getScarseProducts(int quantity);
+	Optional<Product> getProduct(int productId);
+	Product save(Product product);
+	void delete(int productId);
+}
